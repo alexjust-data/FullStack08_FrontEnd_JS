9f6107c0d04987d435d63c91f454b3c0025115ae
Alex Just <alexjustdata@gmail.com>
2023-10-11 19:04:41 +0200

    feat: tema eventos

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 import { tweetListController } from "./tweet-list/tweetListController.js";
 
 
+// // Encuentra y selecciona el botón con el ID 'loadTweets' en el documento HTML.
+// const loadTweetsButton = document.querySelector('#loadTweets');
+
+// // Añade un escuchador de eventos al botón para detectar los clics. "addEventListener('click', function)""
+// loadTweetsButton.addEventListener('click', () => {
+
+//     // Encuentra y selecciona un elemento con el ID 'tweets' en el documento.
+//     const tweetlist = document.getElementById('tweets');
+
+//     // Ejecuta la función 'tweetListController' pasándole el elemento 'tweetlist' como argumento.
+//     tweetListController(tweetlist); // ejecuto el controlador
+// });
+
+
+
+
+const loadTweetsButton = document.querySelector('#loadTweets'); // #almohadillapq es id= en <button id="loadTweets">Cargar tweets</button>
+const removeTweetsButton = document.querySelector('#removeTweets');
+const hideTweetsButton = document.querySelector('#hideTweets');
+
 const tweetlist = document.getElementById('tweets');
-tweetListController(tweetlist); // ejecuto el controlador
 
+loadTweetsButton.addEventListener('click', () => {
+    tweetListController(tweetlist); // ejecuto el controlador en <section id="tweets"></section>
+});
 
-const oldtweetlist = document.getElementById('tweets');
-tweetListController(oldtweetlist); // ejecuto el controlador
+removeTweetsButton.addEventListener('click', () => {
+    tweetlist.innerHTML = ""; // vacío <section id="tweets"></section>
+});
+
+hideTweetsButton.addEventListener('click', () => {
+    tweetlist.classList.toggle('hidden'); // hidden es un nombre comun para la clase
+});
+/** LE DOY STiLo EN <style> del html
+ * #tweet.hidden {
+ *      display: none; 
+ * }
+ */
