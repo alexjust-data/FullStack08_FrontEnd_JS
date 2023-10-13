7d8636c1a489838c15fe46c20d2a95f1ffab5cbb
Alex Just <alexjustdata@gmail.com>
2023-10-13 20:17:35 +0200

    feat: refactoring

--- index.js
+++ index.js
@@ -1,61 +1,46 @@
+// Importaciones:
+// Se importa el controlador de la lista de tweets, 
+// responsable de manejar la lógica relacionada con la lista de tweets.
 import { tweetListController } from "./tweet-list/tweetListController.js";
+// Se importa el controlador de notificaciones, 
+//responsable de mostrar mensajes de notificación en la interfaz.
 import { notificationController } from "./notifications/notificationsControler.js";
 
-
-// // Encuentra y selecciona el botón con el ID 'loadTweets' en el documento HTML.
-// const loadTweetsButton = document.querySelector('#loadTweets');
-
-// // Añade un escuchador de eventos al botón para detectar los clics. "addEventListener('click', function)""
-// loadTweetsButton.addEventListener('click', () => {
-
-//     // Encuentra y selecciona un elemento con el ID 'tweets' en el documento.
-//     const tweetlist = document.getElementById('tweets');
-
-//     // Ejecuta la función 'tweetListController' pasándole el elemento 'tweetlist' como argumento.
-//     tweetListController(tweetlist); // ejecuto el controlador
-// });
-
-
-
-
-const loadTweetsButton = document.querySelector('#loadTweets'); // #almohadillapq es id= en <button id="loadTweets">Cargar tweets</button>
+// Selección de Elementos DOM:
+// Se seleccionan y almacenan en constantes los botones y contenedores relacionados con las acciones de tweets y notificaciones.
+const loadTweetsButton = document.querySelector('#loadTweets');
 const removeTweetsButton = document.querySelector('#removeTweets');
 const hideTweetsButton = document.querySelector('#hideTweets');
-
 const tweetlist = document.getElementById('tweets');
 const notifications = document.getElementById('notifications');
 
-// cuando el usuario a creado el tweet
+// Inicialización:
+// Se inicializa el controlador de notificaciones, pasando el elemento DOM de notificaciones como argumento.
+// La función devuelve una función `showNotification` que se utilizará para mostrar notificaciones.
 const showNotification = notificationController(notifications);
 
+// Event Listeners:
+// Se añade un listener al botón 'loadTweets'. 
+// Al hacer clic en él, se llama al controlador de la lista de tweets.
 loadTweetsButton.addEventListener('click', () => {
-    tweetListController(tweetlist); // ejecuto el controlador en <section id="tweets"></section>
+    tweetListController(tweetlist);
 });
 
+// Se añade un listener al botón 'removeTweets'. 
+// Al hacer clic en él, se limpia el contenido interno de la lista de tweets.
 removeTweetsButton.addEventListener('click', () => {
-    tweetlist.innerHTML = ""; // vacío <section id="tweets"></section>
+    tweetlist.innerHTML = "";
 });
 
+// Se añade un listener al botón 'hideTweets'. 
+// Al hacer clic en él, se alterna la clase 'hidden' del elemento de la lista de tweets.
+// Esto probablemente oculta o muestra la lista de tweets en la interfaz.
 hideTweetsButton.addEventListener('click', () => {
-    tweetlist.classList.toggle('hidden'); // hidden es un nombre comun para la clase
+    tweetlist.classList.toggle('hidden');
 });
-/** LE DOY STiLo EN <style> del html
- * #tweet.hidden {                      <-- fíjate que pongo almohadilla pq section es id=
- *      display: none; 
- * }
- */
 
-
-// estamos escuchando la recepcion del CustomEvent('tweetsLoaded')
-// cuando el usuario a creado el tweet
+// Se añade un listener personalizado al elemento de la lista de tweets para el evento 'tweetsLoaded'.
+// Cuando se dispare este evento, se mostrará una notificación utilizando la información proporcionada en el objeto del evento.
 tweetlist.addEventListener('tweetsLoaded', (event) => {
-    showNotification(event.detail.message)
-})
-/**
- * event.detail.message --> viene de tweetListController.js
- *         del evento "tweetsLoaded", {
-            detail: {
-                type: 'sucess',
-                message: 'Tweets cargados correctamente'
-            }
- */
+    showNotification(event.detail.message, event.detail.type);
+});
